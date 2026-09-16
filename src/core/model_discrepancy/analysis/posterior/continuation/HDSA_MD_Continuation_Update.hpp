05d41866a3caaf3181ea585f06c7f518de33923f
Madhusudan Madhavan <128986723+madhusudan-madhavan@users.noreply.github.com>
2026-09-16 11:14:03 -0600

    Added verbosity control

--- src/core/model_discrepancy/analysis/posterior/continuation/HDSA_MD_Continuation_Update.hpp
+++ src/core/model_discrepancy/analysis/posterior/continuation/HDSA_MD_Continuation_Update.hpp
@@ -37,6 +37,7 @@ namespace HDSA
     int r_;
     RealT grad_tol_;
     bool discard_cache_;
+    int verbosity_;
 
     void Posterior_Update_Core(HDSA::Vector<RealT> &u_k, HDSA::Vector<RealT> &z_k, HDSA::Vector<RealT> &beta_k, int sample_idx) const
     {
@@ -51,7 +52,7 @@ namespace HDSA
 
       HDSA::Ptr<HDSA::PC_Sensitivity_Operator_Interface<RealT>> base_sen_op = Teuchos::rcp_dynamic_cast<HDSA::PC_Sensitivity_Operator_Interface<RealT>>(sen_op);
       HDSA::Ptr<HDSA::PC_Pseudo_Time_Continuation<RealT>> pt_cont =
-          HDSA::makePtr<HDSA::PC_Pseudo_Time_Continuation<RealT>>(beta_nom, base_sen_op, qn_prec, grad_tol_);
+          HDSA::makePtr<HDSA::PC_Pseudo_Time_Continuation<RealT>>(beta_nom, base_sen_op, qn_prec, grad_tol_, true, false, false, static_cast<RealT>(1.e-5), 100, verbosity_);
 
       HDSA::MD_Discrepancy_Parameter_Trajectory<RealT> theta_traj(num_continuation_steps_, sample_idx);
 
@@ -72,8 +73,8 @@ namespace HDSA
                            const HDSA::Ptr<HDSA::MD_z_Prior_Interface<RealT>> &z_prior_interface,
                            const HDSA::Ptr<HDSA::MD_Opt_Prob_Interface<RealT>> &opt_prob_interface, const HDSA::Ptr<HDSA::MD_Posterior_Sampling<RealT>> &post_sampling,
                            const HDSA::Ptr<HDSA::MD_Hessian_Analysis<RealT>> &hessian_analysis, const HDSA::Ptr<HDSA::Random_Number_Generator<RealT>> &random_number_generator,
-                           const int num_continuation_steps, const RealT grad_tol, bool discard_cache = true) : data_interface_(data_interface), u_prior_interface_(u_prior_interface), z_prior_interface_(z_prior_interface), opt_prob_interface_(opt_prob_interface),
-                                                         post_sampling_(post_sampling), hessian_analysis_(hessian_analysis), random_number_generator_(random_number_generator), num_continuation_steps_(num_continuation_steps), grad_tol_(grad_tol), discard_cache_(discard_cache)
+                           const int num_continuation_steps, const RealT grad_tol, bool discard_cache = true, int verbosity = 0) : data_interface_(data_interface), u_prior_interface_(u_prior_interface), z_prior_interface_(z_prior_interface), opt_prob_interface_(opt_prob_interface),
+                                                         post_sampling_(post_sampling), hessian_analysis_(hessian_analysis), random_number_generator_(random_number_generator), num_continuation_steps_(num_continuation_steps), grad_tol_(grad_tol), discard_cache_(discard_cache), verbosity_(verbosity)
     {
       u_opt_ = data_interface_->Get_u_opt()->Clone();
       u_opt_->Set(*data_interface_->Get_u_opt());
